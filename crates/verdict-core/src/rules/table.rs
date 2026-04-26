5e65893bbe6c8d846c76cf3aa0be61de44d51730
kirillkruglikov <kirillkruglikov@gmail.com>
2026-04-26 20:39:06 +0300

    add TableConstraint enum TableRule and TableRuleBuilder

--- crates/verdict-core/src/rules/table.rs
+++ crates/verdict-core/src/rules/table.rs
@@ -1,7 +1,7 @@
 use std::fmt::Display;
 
 #[derive(Debug, Clone)]
-enum TableConstraint {
+pub enum TableConstraint {
     RowsCountBetween { min: usize, max: usize },
     RowsCountGreaterOrEqual(usize),
     RowCountGreaterThan(usize),
@@ -20,7 +20,40 @@ enum TableConstraint {
 
 impl Display for TableConstraint {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        todo!()
+        match self {
+            TableConstraint::ShapeEquals { rows, columns } => {
+                write!(f, "shape_equals({}, {})", rows, columns)
+            }
+            TableConstraint::ColumnsExist(cols) => write!(f, "columns_exists: {:?}", cols),
+            TableConstraint::ColumnsCountLessThan(count) => {
+                write!(f, "columns_count_less_than: {}", count)
+            }
+            TableConstraint::ColumnsCountLessOrEqual(count) => {
+                write!(f, "columns_count_less_or_equal: {}", count)
+            }
+            TableConstraint::ColumnsCountGreaterThan(count) => {
+                write!(f, "columns_count_greater_than: {}", count)
+            }
+            TableConstraint::ColumnsCountGreaterOrEqual(count) => {
+                write!(f, "columns_count_greater_or_equal: {}", count)
+            }
+            TableConstraint::ColumnsCountBetween { min, max } => {
+                write!(f, "columns_count_between({}, {})", min, max)
+            }
+            TableConstraint::RowCountLessThan(count) => write!(f, "row_count_less_than: {}", count),
+            TableConstraint::RowsCountLessOrEqual(count) => {
+                write!(f, "rows_count_less_or_equal: {}", count)
+            }
+            TableConstraint::RowsCountBetween { min, max } => {
+                write!(f, "rows_count_between({}, {})", min, max)
+            }
+            TableConstraint::RowCountGreaterThan(count) => {
+                write!(f, "row_count_greater_than: {}", count)
+            }
+            TableConstraint::RowsCountGreaterOrEqual(count) => {
+                write!(f, "rows_count_greater_or_equal: {}", count)
+            }
+        }
     }
 }
 
@@ -29,15 +62,32 @@ pub struct TableRule {
     pub constraint: TableConstraint,
 }
 
+impl TableRule {
+    pub fn new(constraint: TableConstraint) -> TableRule {
+        TableRule { constraint }
+    }
+}
+
 #[derive(Default)]
 pub struct TableRuleBuilder {
     pub constraint: Vec<TableConstraint>,
 }
 
 impl TableRuleBuilder {
-    fn shape_equals(mut self, rows: usize, columns: usize) -> Self {
+    pub fn shape_equals(mut self, rows: usize, columns: usize) -> Self {
         self.constraint
             .push(TableConstraint::ShapeEquals { rows, columns });
         self
     }
+
+    pub fn build(self) -> Vec<TableRule> {
+        self.constraint
+            .into_iter()
+            .map(|c| TableRule { constraint: c })
+            .collect()
+    }
+}
+
+pub fn table_rule() -> TableRuleBuilder {
+    TableRuleBuilder { constraint: vec![] }
 }
